90d93ad173aa93d695d2cdedf28c1feadc2e772c
ecMode <echen.contact@gmail.com>
2014-06-17 21:35:52 -0400

    Adding create page and button.

---
+++ Assets/Scripts/CharacterSelectionBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelectionBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown (){
+		if ( Input.GetMouseButtonDown(0)){
+			Application.LoadLevel("Home");
+		}
+	}
+}
